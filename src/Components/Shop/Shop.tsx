8f4dca8ce79a69ded171912cc52b0dbed05f1ee2
muhamash <ashraful@xpeedlab.tech>
2024-01-23 23:54:11 +0600

    working on shop

--- src/Components/Shop/Shop.tsx
+++ src/Components/Shop/Shop.tsx
@@ -1,13 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+
+interface Product {
+    [ key: string ]: string;
+}
+
+const Loading = () => (
+  <div className='text-lg font-mono text-red-600'>Loading...</div>
+);
 
 const Shop = () =>
 {
-    const initialProducts = useLoaderData()
-    console.log(typeof initialProducts)
+    const initialProducts: any = useLoaderData();
+    const [ loading, setLoading ] = useState( true );
+
+    const fetchData = async () =>
+    {
+        try
+        {
+            await new Promise( resolve => setTimeout( resolve, 500 ) );
+            setLoading( false );
+        }
+        catch ( error )
+        {
+            console.error( 'Error fetching products:', error );
+            setLoading( false );
+        }
+    };
+
+    useEffect( () =>
+    {
+        fetchData();
+    }, [] );
+
     return (
         <div>
-            Shop:{}
+            {loading ? (
+                <Loading />
+            ) : (
+                <div>
+                    Shop: {initialProducts.length}
+                    <div>
+                        {initialProducts.map( ( product: Product, index: number ) => (
+                            <div key={index}>
+                                <img className='w-[200px] h-[200px]' src={product.image} alt="" />
+                                <p>{product.price}TK</p>
+                            </div>
+                        ) )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
